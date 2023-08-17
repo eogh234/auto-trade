41b27533f2db9f8e5ad95b53f568b5e5b977fb21
Daeho <eogh234@naver.com>
2023-08-18 01:35:25 +0900

    Add Symbol Name Print

--- parsing.py
+++ parsing.py
@@ -3,6 +3,9 @@ from bs4 import BeautifulSoup as bs
 
 
 def get_best_list():
+    symbol_name_list = []
+    symbol_code_list = []
+
     url = requests.get("https://finance.naver.com/")
 
     soup = bs(url.text, "html.parser")
@@ -11,8 +14,13 @@ def get_best_list():
     best_list = []
 
     for elem in elements:
-        best_list.append(elem.select_one('a')['href'].split('=')[1])
+        best_list.append(
+            [elem.find('a').text, elem.select_one('a')['href'].split('=')[1]])
 
-    print(best_list)
+    for name, code in best_list:
+        symbol_name_list.append(name)
+        symbol_code_list.append(code)
 
-    return best_list
+    print(symbol_name_list)
+
+    return symbol_code_list
